d49858eb083a1c5355887af6f5ac95dffcba7ad3
NOFUCIDKDJDK <152839808+NOFUCIDKDJDK@users.noreply.github.com>
2024-01-27 22:51:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,5 @@ const Discord = require('discord.js'); //import client from discord
 
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-    }
-});
 
 client.login(process.env.TOKEN); //login bot using token
